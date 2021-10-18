fe425db4585ea792e82e69cbd0c93eab0dca7d36
fish-paulo <fish.paulo@gmail.com>
2021-10-18 20:05:07 -0300

    Create palindromo.js

---
+++ basecamp js/palindromo.js
@@ -0,0 +1,25 @@
+
+
+function varificaPalindromo2(string) {
+	if (!string) return;
+	if (!string.length) return;
+
+	for (var i = 0; i < string.length / 2; i++) {
+		if (string[i] !== string[string.length - 1 - i]) {
+			return console.log(false);
+		}
+	}
+	return console.log(true);
+}
+
+varificaPalindromo2('carro');
+varificaPalindromo2('ovo');
+varificaPalindromo2('arara');
+varificaPalindromo2('gato');
+varificaPalindromo2('osso');
+
+
+
+
+
+
